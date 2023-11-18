0812ddd38d74d0b853d62f43b29232a473da226e
Alfredo Lorentiar S <alorentiar03@gmail.com>
2023-11-18 15:31:12 +0700

    retrieving product migration that previously deleted

---
+++ database/migrations/2023_11_18_151942_create_products_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->foreignId('category_id');
+            $table->foreignId('brand_id');
+            $table->foreignId('supplier_id');
+            $table->foreignId('location_id');
+            $table->foreignId('tax_rate_id');
+            $table->string('product_name');
+            $table->string('simple_name');
+            $table->string('sku');
+            $table->string('upc_ean');
+            $table->string('supplier_pid');
+            $table->string('price');
+            $table->string('stock');
+            $table->string('quantity');
+            $table->string('description');
+            $table->string('status');
+            $table->string('image');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
