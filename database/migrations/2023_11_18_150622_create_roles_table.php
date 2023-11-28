eae979daff19f6d804e6f5aac95cb6ba53b664a2
Alfredo Lorentiar S <alorentiar03@gmail.com>
2023-11-29 06:01:00 +0700

    migration roles table

--- database/migrations/2023_11_18_150622_create_roles_table.php
+++ database/migrations/2023_11_18_150622_create_roles_table.php
@@ -15,6 +15,51 @@ return new class extends Migration
             $table->id();
             $table->timestamps();
             $table->string('role_name');
+            //Home
+            $table->tinyinteger('home_overview')->nullable();
+            $table->tinyinteger('home_upcoming_booking')->nullable();
+            //Calendar
+            $table->tinyInteger('calendar_calendar')->nullable();
+            $table->tinyInteger('calendar_create_booking')->nullable();
+            $table->tinyInteger('calendar_list_booking')->nullable();
+            //Staff
+            $table->tinyInteger('staff_dashboard')->nullable();
+            $table->tinyInteger('staff_staff_list')->nullable();
+            $table->tinyInteger('staff_job')->nullable();
+            $table->tinyInteger('staff_access_control')->nullable();
+            $table->tinyInteger('staff_security_groups')->nullable();
+            //Service
+            $table->tinyInteger('service_dashboard');
+            $table->tinyInteger('service_list');
+            $table->tinyInteger('service_treatment_plan');
+            $table->tinyInteger('service_category');
+            $table->tinyInteger('service_diagnosis');
+            $table->tinyInteger('service_policy');
+            //Product
+            $table->tinyInteger('product_dashboard');
+            $table->tinyInteger('product_list');
+            $table->tinyInteger('product_brand');
+            $table->tinyInteger('product_category');
+            $table->tinyInteger('product_suppliers');
+            //Location
+            $table->tinyInteger('dashboard_location');
+            $table->tinyInteger('location_list');
+            $table->tinyInteger('facilities');
+            $table->tinyInteger('setting_location');
+            //Finance
+            $table->tinyInteger('dashboard_finance');
+            $table->tinyInteger('sale_list');
+            $table->tinyInteger('quotation_list');
+            $table->tinyInteger('tax_rate');
+            //Attendance
+            $table->tinyInteger('dashboard_attendance');
+            $table->tinyInteger('attendance_list');
+            $table->tinyInteger('working_shift');
+            $table->tinyInteger('manage_staff_shift');
+            //Presence
+            $table->tinyInteger('absent');
+            //Reports
+            $table->tinyInteger('reports_all');
         });
     }
 
